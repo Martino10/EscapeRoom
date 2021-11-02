9b0cd33f1a6a2cf25977d1cea8d3b2d4041d9d08
DennisLeyting <dennisleyting@gmail.com>
2021-11-02 20:09:55 +0100

    puzzle 1

---
+++ EscapeRoom/js/puzzle1.js
@@ -0,0 +1,62 @@
+const answer = ["4", "2", "6", "1", "7"]
+
+invulveld__1 = document.getElementById("invulveld1__js")
+invulveld__2 = document.getElementById("invulveld2__js")
+invulveld__3 = document.getElementById("invulveld3__js")
+invulveld__4 = document.getElementById("invulveld4__js")
+invulveld__5 = document.getElementById("invulveld5__js")
+hint__scherm = document.getElementById("hints__js")
+
+function delete__input(){
+    invulveld__1.innerHTML = ""
+    invulveld__2.innerHTML = ""
+    invulveld__3.innerHTML = ""
+    invulveld__4.innerHTML = ""
+    invulveld__5.innerHTML = ""
+    console.log("yeet")
+}
+
+
+function invullen(getal){
+    console.log("heh?")
+    if (invulveld__1.innerHTML == ""){
+        invulveld__1.innerHTML = getal
+        console.log("veld1")
+    } else if (invulveld__2.innerHTML == ""){
+        invulveld__2.innerHTML = getal
+    } else if (invulveld__3.innerHTML == ""){
+        invulveld__3.innerHTML = getal
+    } else if (invulveld__4.innerHTML == ""){
+        invulveld__4.innerHTML = getal
+    } else if (invulveld__5.innerHTML == ""){
+        invulveld__5.innerHTML = getal
+        if (check__input() == false) {
+            hint__scherm.innerHTML += invulveld__1.innerHTML + invulveld__2.innerHTML + invulveld__3.innerHTML + invulveld__4.innerHTML + invulveld__5.innerHTML + 
+            " ==> " + maakhint(invulveld__1.innerHTML, invulveld__2.innerHTML, invulveld__3.innerHTML, invulveld__4.innerHTML, invulveld__5.innerHTML) + "<br>"
+        }
+    } 
+}
+
+function check__input(){
+    if (invulveld__1.innerHTML == answer[0] && invulveld__2.innerHTML == answer[1] && invulveld__3.innerHTML == answer[2] && invulveld__4.innerHTML == answer[3] && invulveld__5.innerHTML == answer[4]){
+        console.log("yay")
+    } else {
+        return false
+    }
+}
+
+function maakhint(getal1, getal2, getal3, getal4, getal5){
+    let CC = 0
+    let CW = 0
+    const input = [getal1, getal2, getal3, getal4, getal5]
+    for (let i = 0; i < input.length; i++){
+        if (input[i] == answer[i]){
+            CC += 1
+        } else if (answer.includes(input[i])){
+            CW += 1
+
+        }
+    }
+    delete__input()
+    return(CW + "CW & " + CC + "CC ")
+}

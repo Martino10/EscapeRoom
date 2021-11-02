b28f6f55faec9fffd7370f6a4ddd13b4badebf91
anne <annegroenewoud1@gmail.com>
2021-11-02 14:39:43 +0100

    dialoog update

--- EscapeRoom/js/timer.js
+++ EscapeRoom/js/timer.js
@@ -155,10 +155,10 @@ window.addEventListener("load", () => {
 
 
     window.portraitchecker = setInterval(doPortraitCheck, 500)
-    if (isTimerPaused()) {
-        document.getElementById('time__js').style = 'color: gray;';
-    } else {
-        document.getElementById('skip__knop__js').disabled = true;
-    }
+    // if (isTimerPaused()) {
+    //     document.getElementById('time__js').style = 'color: gray;';
+    // } else {
+    //     document.getElementById('skip__knop__js').disabled = true;
+    // }
 }
 , true)

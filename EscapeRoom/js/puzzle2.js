85dd355acd1613103bcee1e4ac59fd0b36bf2280
Martin Krikke <martin.krikke10@gmail.com>
2021-11-30 15:53:02 +0100

    puzzle 2 updates

--- EscapeRoom/js/puzzle2.js
+++ EscapeRoom/js/puzzle2.js
@@ -8,32 +8,31 @@ buttonClicked = {
     6: false
 }
 clickOrder = [];
+correct = [1, 2, 3, 4, 5, 6];
 
 const checkAll = () => {
-    // Check if buttons were pushed in the right order once all buttons are clicked
-    if (buttonClicked[1] && buttonClicked[2] && buttonClicked[3] && buttonClicked[4] && buttonClicked[5] && buttonClicked[6]) {
-        if (clickOrder[0] == 1 && clickOrder[1] == 2 && clickOrder[2] == 3 && clickOrder[3] == 4 && clickOrder[4] == 5 && clickOrder[5] == 6) {
-            location.href = 'puzzle2-dialoog2.html';
-            return;
-        }
-        else { console.log("try again.");}
-
-        //reset buttons
-        setTimeout(() => { for (i = 1; i <= 6; i++) {
-            document.getElementById("buttonimg" + i).src = "img/button_unpressed.png";
-            document.getElementById("js__button" + i).disabled = false;
-            
-        } }, 1000);
-        buttonClicked = {
-            1: false,
-            2: false,
-            3: false,
-            4: false,
-            5: false,
-            6: false
-        }
-        clickOrder = [];
+    // Check if buttons were pushed in the right order
+    if (clickOrder.every((val, index) => val === correct[index])) {
+        location.href = 'puzzle2-dialoog2.html';
+        return;
     }
+    else { console.log("try again.");}
+
+    //reset buttons
+    setTimeout(() => { for (i = 1; i <= 6; i++) {
+        document.getElementById("buttonimg" + i).src = "img/button_unpressed.png";
+        document.getElementById("js__button" + i).disabled = false;
+        
+    } }, 1000);
+    buttonClicked = {
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false
+    }
+    clickOrder = [];
 }
 
 const button_clicked = (num) => {
@@ -41,6 +40,15 @@ const button_clicked = (num) => {
     document.getElementById("js__button" + num).disabled = true;
     buttonClicked[num] = true;
     clickOrder.push(num);
+    check_allClicked();
+}
+
+const check_allClicked = () => {
+    for (i = 1; i < 7; i++) {
+        if (buttonClicked[i] == false) {
+            return;
+        }
+    }
     checkAll();
 }
 
